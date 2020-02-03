de41aff4e5f5a9238bf4b71d850b438e70089da0
Ricky Rozier <rickydrozier@gmail.com>
2020-02-03 03:52:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,5 +2,5 @@ def badge_maker(name)
  puts "Hello, my name is #{name}."
  end
 def 
-batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+batch_badge_creator["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 end
